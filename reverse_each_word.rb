824e761809196c4bbdb3e59b5e6affaefc61b328
Nadia Lee <miss.nadia.lee@gmail.com>
2017-12-19 21:35:39 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,5 +3,5 @@ def reverse_each_word(sentence)
 end
 
 def reverse_each_word(sentence)
-  sentence.split.collect { |letter|.reverse }.join(" ")
+  sentence.split.collect { |letter| letter.reverse }.join(" ")
 end
